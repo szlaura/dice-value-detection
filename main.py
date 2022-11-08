f62726bdc6bb20a5217deda787b69816e018c674
Szekeres Laura <h880629@stud.u-szeged.hu>
2022-11-08 11:19:25 +0100

    update algorithm with blob detection

--- main.py
+++ main.py
@@ -3,35 +3,52 @@ import numpy as np
 
 
 def main():
-    file_name = 'dice02.png'
-
-
+    file_name = "dice04.jpg"
     src = cv.imread(file_name, cv.IMREAD_COLOR)
+
     # Check if image is loaded fine
     if src is None:
         print('Error opening image!')
         return -1
 
-    dim = 360
-    src = cv.resize(src, (dim, dim), interpolation=cv.INTER_AREA)
-
     gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
-    gray = cv.medianBlur(gray, 3)
 
-    rows = gray.shape[0]
-    dots = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 16,
-                              param1=100, param2=20,
-                              minRadius=1, maxRadius=20)
+    # Set our filtering parameters
+    # Initialize parameter setting using cv2.SimpleBlobDetector
+    params = cv.SimpleBlobDetector_Params()
 
-    if dots is not None:
-        dots = np.uint16(np.around(dots))
-        for coords in dots[0, :]:
-            center, radius = (coords[0], coords[1]), coords[2]
-            cv.circle(src, center, radius, (0, 255, 0), -1)
-        print("Number of detected dice dots: ", len(dots[0, :]))
+    params.filterByArea = True
+    params.minArea = 300
 
-    cv.imshow("detected dice dots", src)
+    params.filterByCircularity = True
+    params.minCircularity = 0.2
+
+    params.filterByConvexity = True
+    params.minConvexity = 0.2
+
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.014
+    params.maxInertiaRatio = 0.4
+
+    # Create a detector with the parameters
+    detector = cv.SimpleBlobDetector_create(params)
+
+    # Detect blobs
+    keypoints = detector.detect(gray)
+
+    # Draw blobs on our image as red circles
+    blank = np.zeros((1, 1))
+    blobs = cv.drawKeypoints(gray, keypoints, blank, (0, 0, 255),
+                             cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    text = "Dots number: " + str(len(keypoints))
+    print(text)
+
+    cv.putText(blobs, text, (3, 30), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+    cv.imshow("Rolling dice", blobs)
     cv.waitKey(0)
+    cv.destroyAllWindows()
 
     return 0
 
